5e94f78f608a0bfba9914bfc17b43be5932e0913
Alana McPhail <alanamcphail@Alanas-MacBook-Air.local>
2022-06-07 18:06:47 -0400

    practice

---
+++ practice.R
@@ -0,0 +1,19 @@
+
+# Github URL
+https://github.com/alanamcphail22/DHT22Practice
+
+
+
+# Overview of operators 
+# Mod gives remainder from division 
+5 %% 2
+
+5 %/% 2
+
+install.packages("magrittr")
+
+library(magrittr)
+
+1:5 %>% sqrt()
+
+
